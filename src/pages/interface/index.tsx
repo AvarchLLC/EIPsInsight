b94f8583cef588580121d09319258ef9d84f56e2
nandanpi <nandanrp09@gmail.com>
2023-11-20 00:42:23 +0530

    fixed some old category pages

--- src/pages/interface/index.tsx
+++ src/pages/interface/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 import AllLayout from "@/components/Layout";
-import { Box, Button } from '@chakra-ui/react'
-import FlexBetween from '@/components/FlexBetween'
-import Header from '@/components/Header'
-import { DownloadIcon } from '@chakra-ui/icons'
-import Table from '@/components/Table'
-import LineChart from '@/components/LineChart'
-import TableStatus from '@/components/TableStatus'
-import LineStatus from '@/components/LineStatus'
-import AreaStatus from '@/components/AreaStatus';
-import { useEffect, useState } from 'react'
-import { motion } from 'framer-motion';
-import LoaderComponent from '@/components/Loader';
+import { Box, Button } from "@chakra-ui/react";
+import FlexBetween from "@/components/FlexBetween";
+import Header from "@/components/Header";
+import { DownloadIcon } from "@chakra-ui/icons";
+import Table from "@/components/Table";
+import LineChart from "@/components/LineChart";
+import TableStatus from "@/components/TableStatus";
+import LineStatus from "@/components/LineStatus";
+import AreaStatus from "@/components/AreaStatus";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import LoaderComponent from "@/components/Loader";
 interface EIP {
   _id: string;
   eip: string;
@@ -39,7 +39,7 @@ const Interface = () => {
         setData(jsonData);
         setIsLoading(false); // Set loader state to false after data is fetched
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setIsLoading(false); // Set loader state to false even if an error occurs
       }
     };
@@ -58,7 +58,7 @@ const Interface = () => {
   }, []);
   return (
     <AllLayout>
-            {isLoading ? ( // Check if the data is still loading
+      {isLoading ? ( // Check if the data is still loading
         // Show loader if data is loading
         <Box
           display="flex"
@@ -77,34 +77,43 @@ const Interface = () => {
         </Box>
       ) : (
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-    <Box className="ml-40 mr-40 pl-10 pr-10 mt-10 mb-20">
-      <FlexBetween>
-      <Header title={`Standard Tracks - Interface [ ${data.filter((item) => item.type === "Standards Track" && item.category === "Interface").length} ]`} subtitle="Interface EIPs describe changes to the Ethereum client API." />
-        <Box>
-          <Button
-            colorScheme="blue"
-            variant="outline"
-            fontSize={"14px"}
-            fontWeight={"bold"}
-            padding={"10px 20px"}
-          >
-            <DownloadIcon marginEnd={"1.5"} />
-            Download Reports
-          </Button>
-        </Box>
-      </FlexBetween>
-      <TableStatus cat='Interface'/>
-      <AreaStatus />
-    </Box>
-    </motion.div>
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Box className="ml-40 mr-40 pl-10 pr-10 mt-10 mb-20">
+            <FlexBetween>
+              <Header
+                title={`Standard Tracks - Interface [ ${
+                  data.filter(
+                    (item) =>
+                      item.type === "Standards Track" &&
+                      item.category === "Interface"
+                  ).length
+                } ]`}
+                subtitle="Interface EIPs describe changes to the Ethereum client API."
+              />
+              <Box>
+                <Button
+                  colorScheme="blue"
+                  variant="outline"
+                  fontSize={"14px"}
+                  fontWeight={"bold"}
+                  padding={"10px 20px"}
+                >
+                  <DownloadIcon marginEnd={"1.5"} />
+                  Download Reports
+                </Button>
+              </Box>
+            </FlexBetween>
+            <TableStatus cat="Interface" />
+            <AreaStatus type={"EIPs"} />
+          </Box>
+        </motion.div>
       )}
-  </AllLayout>
-  )
-}
+    </AllLayout>
+  );
+};
 
-export default Interface
+export default Interface;
