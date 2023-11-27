78024d26f70de11835a7bd8b898fb43bbde0ec2d
nandanpi <nandanrp09@gmail.com>
2023-11-27 11:48:14 +0530

    fix: Updated resource links

--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import AllLayout from "@/components/Layout";
 import Resources from "@/components/Resource";
 import FeaturedVideos from "@/components/FeaturedVideos";
-import {useColorModeValue} from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra-ui/react";
 
 const Index = () => {
   const bg = useColorModeValue("#f6f6f7", "#171923");
-    return (
-        <AllLayout>
-            <Resources title="Featured Videos" summary="by EtherWorld" link='https://www.youtube.com/watch?v=fwxkbUaa92w'/>
-        </AllLayout>
-    );
-}
+  return (
+    <AllLayout>
+      <Resources />
+    </AllLayout>
+  );
+};
 export default Index;
