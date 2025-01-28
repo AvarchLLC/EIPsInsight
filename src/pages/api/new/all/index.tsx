a835d85efa12eb8e4675b455c3b06b3a124fbab3
Viwin Kumar Padala <viwinkumar.p20@iiits.in>
2025-01-28 20:47:10 +0530

    fix compilation issues

--- src/pages/api/new/all/index.tsx
+++ src/pages/api/new/all/index.tsx
@@ -26,11 +26,11 @@ const mdFilesSchema = new mongoose.Schema({
 });
 
 const EIPMdFiles =
-  mongoose.models.EipMdFiles || mongoose.model("EipMdFiles3", mdFilesSchema);
+  mongoose.models.EipMdFiles3 || mongoose.model("EipMdFiles3", mdFilesSchema);
 const ERCMdFiles =
-  mongoose.models.ErcMdFiles || mongoose.model("ErcMdFiles3", mdFilesSchema);
+  mongoose.models.ErcMdFiles3 || mongoose.model("ErcMdFiles3", mdFilesSchema);
 const RIPMdFiles =
-  mongoose.models.RipMdFiles || mongoose.model("RipMdFiles3", mdFilesSchema);
+  mongoose.models.RipMdFiles3 || mongoose.model("RipMdFiles3", mdFilesSchema);
 
 // export default async (req: Request, res: Response) => {
 // const EIPResult = await EIPMdFiles.aggregate([
