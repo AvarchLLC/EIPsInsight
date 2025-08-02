2bd03a4b8444b5c65b5befd8b9148946d1643b54
Dhanush Naik <dhanushlnaik@gmail.com>
2025-08-03 00:31:24 +0530

    Image Props fix

--- src/components/BorderImage.tsx
+++ src/components/BorderImage.tsx
@@ -1,13 +1,15 @@
-// BorderedImage.tsx
-"use client";
+import { Box, Image as ChakraImage, useColorModeValue, ImageProps, BoxProps } from '@chakra-ui/react';
 
-import { Box, Image as ChakraImage, useColorModeValue } from "@chakra-ui/react";
+type BorderedImageProps = {
+  src: string;
+  alt?: string;
+} & BoxProps; // or ImageProps if you want to pass to the image itself
 
-export default function BorderedImage({ src, alt, ...props }) {
+export default function BorderedImage({ src, alt, ...props }: BorderedImageProps) {
   const borderColor = useColorModeValue('teal.400', 'teal.600');
   return (
     <Box borderWidth="2px" borderColor={borderColor} borderRadius="xl" {...props}>
-      <ChakraImage src={src} alt={alt} width="100%" objectFit="cover" />
+      <ChakraImage src={src} alt={alt ?? ''} width="100%" objectFit="cover" />
     </Box>
   );
 }
